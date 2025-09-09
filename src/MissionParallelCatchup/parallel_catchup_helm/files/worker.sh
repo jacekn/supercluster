a85bb1dfc31e55cad8f4975ba8c4a69316a91dd8
Jacek N <jacek@stellar.org>
2025-09-09 10:54:42 +0100

    changes

--- src/MissionParallelCatchup/parallel_catchup_helm/files/worker.sh
+++ src/MissionParallelCatchup/parallel_catchup_helm/files/worker.sh
@@ -28,20 +28,12 @@ JOB_KEY=$(redis-cli -h "$REDIS_HOST" -p "$REDIS_PORT" LMOVE "$JOB_QUEUE" "$PROGR
 if [ -n "$JOB_KEY" ]; then
     # Start timer
     START_TIME=$(date +%s)  
-    echo "SSC_DEBUG $START_TIME Processing job: $JOB_KEY"
+    START_DATE=$(date)
+    echo "SSC_DEBUG $START_DATE Processing job: $JOB_KEY"
     core_id=$(echo "$POD_NAME" | grep -o '[0-9]\+')
-    redis-cli -h "$REDIS_HOST" -p "$REDIS_PORT" SADD SSC_STATUS "$core_id|$JOB_KEY|$START_TIME"
+    redis-cli -h "$REDIS_HOST" -p "$REDIS_PORT" SADD SSC_STATUS "$core_id|$JOB_KEY|$START_DATE"
 
     if [ ! "$(/usr/bin/stellar-core --conf /config/stellar-core.cfg new-db --console&&
-<<<<<<< HEAD
-    /usr/bin/stellar-core --conf /config/stellar-core.cfg catchup "$JOB_KEY" --metric 'ledger.transaction.apply' --console)" ]; then
-        echo "Error processing job: $JOB_KEY"
-        QUEUE_COMMAND="LPUSH $FAILED_QUEUE \"$JOB_KEY|$POD_NAME\""  # enhance the entry with pod name for tracking
-    else
-        echo "Successfully processed job: $JOB_KEY"
-        QUEUE_COMMAND="LPUSH $SUCCESS_QUEUE \"$JOB_KEY\""
-    fi
-=======
         /usr/bin/stellar-core --conf /config/stellar-core.cfg catchup "$JOB_KEY" --metric 'ledger.transaction.apply' --console)" ]; then
         echo "SSC_DEBUG $(date) Error processing job: $JOB_KEY"
         redis-cli -h "$REDIS_HOST" -p "$REDIS_PORT" LPUSH "$FAILED_QUEUE" "$JOB_KEY|$POD_NAME" # enhance the entry with pod name for tracking
@@ -50,8 +42,6 @@ if [ -n "$JOB_KEY" ]; then
         redis-cli -h "$REDIS_HOST" -p "$REDIS_PORT" LPUSH "$SUCCESS_QUEUE" "$JOB_KEY"
     fi
     redis-cli -h "$REDIS_HOST" -p "$REDIS_PORT" LREM "$PROGRESS_QUEUE" -1 "$JOB_KEY"
-    redis-cli -h "$REDIS_HOST" -p "$REDIS_PORT" SREM SSC_STATUS "$core_id|$JOB_KEY|$START_TIME"
->>>>>>> 6027069 (debug statements)
 
     # Parse and extract the metrics from the log file
     LOG_FILE=$(ls -t $LOG_DIR/stellar-core*.log | head -n 1)
@@ -69,7 +59,6 @@ if [ -n "$JOB_KEY" ]; then
     DURATION=$((END_TIME - START_TIME))s
     echo "SSC_DEBUG $(date) Finish processing job: $JOB_KEY, duration: $DURATION"
 
-<<<<<<< HEAD
     # Push metrics to redis in a transaction to ensure data consistency. Retry for 5min on failures
     core_id=$(echo "$POD_NAME" | grep -o '[0-9]\+')
     for i in $(seq 1 30);do
@@ -90,12 +79,6 @@ EOF
     done
 else
     echo "$(date) No more jobs in the queue. Sleeping for $SLEEP_INTERVAL seconds..."
-=======
-    # push metrics
-    redis-cli -h "$REDIS_HOST" -p "$REDIS_PORT" SADD "$METRICS" "$JOB_KEY|$core_id|$tx_apply_ms|$DURATION"
-else
-    echo "SSC_DEBUG $(date) No more jobs in the queue. Sleeping for $SLEEP_INTERVAL seconds..."
->>>>>>> 6027069 (debug statements)
     sleep $SLEEP_INTERVAL
 fi
 done
